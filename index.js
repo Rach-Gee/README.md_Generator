c4af270998817ef19fa4ce5cd27dde3995628f30
Rach Gee <Rach-Gee@hotmail.com>
2021-07-25 17:58:15 +0800

    adding starter code which includes questions

---
+++ index.js
@@ -0,0 +1,83 @@
+//Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+
+
+//Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: "What is your GitHub username?",
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repo?",
+        name: 'repo',
+    },
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is your project description?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What is your projects installation instructions',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage information',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What are the features of your project',
+        name: 'features',
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines',
+        name: 'guidelines',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    }];
+
+//Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Success! Your README.md file has been generated")
+    });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((response) => {
+    console.log(response);
+    writeToFile('README.md',responses)
+    }
+)}
+
+// Function call to initialize app
+init();
