6f6f3b449f169466db865f574451e628c3c0d6f9
Rach Gee <Rach-Gee@hotmail.com>
2021-07-25 18:21:41 +0800

    fxing response bug

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -18,6 +18,18 @@ ${data.title}
     ## Overview
         
     ${data.description}
+
+    1. [Installation](#Installation)
+
+    2. [Usage](#Usage)
+
+    3. [Features](#Features)
+
+    4. [How to Contribute](#How-to-Contribute)
+
+    5. [Tests](#Tests)
+
+    6. [Questions](#Questions)
         
     ## Installation
         
@@ -36,8 +48,11 @@ ${data.title}
     ${data.guidelines}
         
     ## Tests
-    ${data.test}`
+    ${data.test}
+    
+    ## Questions
+    ${response.questions}`
 
-}
+};
 
 module.exports = generateMarkdown;
